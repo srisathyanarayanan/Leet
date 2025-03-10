4b272ab9391b0ff00781a50124666e6e24b10176
srisathyanarayanan <131900842+srisathyanarayanan@users.noreply.github.com>
2025-03-10 22:56:43 +0530

    Time: 15 ms (5.81%), Space: 44.9 MB (54.74%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,25 +1,35 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-         Map<Integer,Integer> map = new HashMap<>();
-
-        for(int i=0;i<mat.length;i++){
-            int count = 0;
-            for(int j=0;j< mat[i].length;j++){
-                if(mat[i][j]==1){
-                    count++;
+         List<Integer> l =new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        for(int[] i:mat){
+            int c=0;
+            for(int j:i){
+                if(j==1) {
+                    c++;
                 }
-                map.put(i,count);
             }
-
+            l.add(c);
         }
-        List<Map.Entry<Integer,Integer>> sort = new ArrayList<>(map.entrySet());
-        sort.sort(Map.Entry.comparingByValue());
-
+        for(int i=0;i<l.size();i++){
+            l1.add(l.get(i));
+        }
+        Collections.sort(l1);
+        for(int i=0;i<l.size();i++){
+            for(int j=0;j<l.size();j++){
+                if(l2.contains(j)){
+                    continue;
+                }
+                 if(Objects.equals(l1.get(i), l.get(j))){
+                    l2.add(j);
+                }
+            }
+        }
         int[] weak = new int[k];
         for(int i=0;i<k;i++){
-            weak[i] = sort.get(i).getKey();
-
+            weak[i]= l2.get(i);
         }
-    return weak;
+        return weak;
     }
 }
